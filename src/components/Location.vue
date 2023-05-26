6fb6d348ce22ac5cd2b27807d02a6ded4bde390d
RJ <randy.j416@yahoo.com>
2023-05-26 08:42:32 -0400

    add geolocation api to component

---
+++ src/components/Location.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue.js',
+      latitude: '',
+      longitude: '',
+    }
+  },
+  methods: {
+    getLocation(event) {
+      event.preventDefault()
+      navigator.geolocation.getCurrentPosition((position)=> {
+      console.log(position.coords.latitude, position.coords.longitude)
+      this.latitude = position.coords.latitude
+      this.longitude = position.coords.longitude
+      })
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>
+      <button @click="getLocation"> Click me to get location </button>
+    </h3>
+    <div>
+      Your location is: {{latitude}}, {{longitude}}
+    </div>
+  </div>
+</template>
